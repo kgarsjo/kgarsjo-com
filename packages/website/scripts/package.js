948ee99c3164f89ef5a890f5652c38007c832172
kgarsjo <krgarsjo@gmail.com>
2019-08-10 17:24:55 -0700

    Remove generated components, render simple headers

--- packages/website/scripts/package.js
+++
@@ -1,5 +0,0 @@
-const { packageZipToS3 } = require('@kgarsjo-com/packaging');
-packageZipToS3({
-    packageName: 'KgarsjoComWebsite',
-    dirsToBundle: ['./build'],
-});
---
+++ packages/website/src/components/App/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => (
+    <h1>Kevin Garsjo</h1>
+    <h2>Software Engineer</h2>
+);
