b9b960831900ca822b56fc963131d7925cb9c835
kgarsjo <krgarsjo@gmail.com>
2019-12-25 21:19:40 -0800

    [Feature] Create unstyled homepage

--- packages/website/src/components/App/index.tsx
+++ packages/website/src/components/App/index.tsx
@@ -4,6 +4,13 @@ const App = () => (
     <Fragment>
         <h1>Kevin Garsjo</h1>
         <h2>Software Engineer</h2>
+        <p><span role="img" aria-label="work">🏢</span> SDE2 at Amazon</p>
+        <p><span role="img" aria-label="home">🏠</span>{" Living in Portland, OR"}</p>
+        <p><span role="img" aria-label="education">🎓</span>{" Bachelors of Science, CompSci, Universiy of Oregon"}</p>
+        <footer>
+            <div><a href="https://github.com/kgarsjo">GitHub</a></div>
+            <div><a href="https://www.linkedin.com/in/krgarsjo/">LinkedIn</a></div>
+        </footer>
     </Fragment>
 );
 
