f8a161d374cbb688b2310e82d1a6447f7953ad9e
kgarsjo <krgarsjo@gmail.com>
2019-12-28 20:52:58 -0800

    [feat] Create and use an ACM certificate

--- packages/infrastructure/lib/AppStack/index.ts
+++ packages/infrastructure/lib/AppStack/index.ts
@@ -1,16 +1,21 @@
 import { App, Stack, CfnParameter } from '@aws-cdk/core';
 import Website from '../Constructs/Website';
 import { Bucket } from '@aws-cdk/aws-s3';
+import AccountIDConfiguration, { AccountIDConfigMap } from '../Constructs/AccountIDConfiguration';
 
 export interface AppStackConfig {
-    budgetSubscriberEmail: string,
+    accountIDConfiguration: AccountIDConfigMap,
 }
 
 export default class AppStack extends Stack {
-    constructor(scope: App, name: string) {
+    constructor(scope: App, name: string, props: AppStackConfig) {
         super(scope, name);
+        
+        const accountIDConfig = new AccountIDConfiguration(this, props.accountIDConfiguration);
 
         new Website(this, {
+            domainName: accountIDConfig.getDomainName(),
+            sslCertificateARN: accountIDConfig.getSSLCertificateArn(),
             unzipperLambdaBucket: Bucket.fromBucketName(this, 'UnzipperLambdaBucket',
                 new CfnParameter(this, 'UnzipperLambdaArtifactBucket', {
                     type: 'String',
